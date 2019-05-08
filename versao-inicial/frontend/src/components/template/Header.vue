40f975c8f3bfa69d08cd92f6893830b35e22924d
LuisClaudio-PC <wowluisjnr@gmail.com>
2019-05-08 20:36:39 -0300

    layout header finalizado

--- versao-inicial/frontend/src/components/template/Header.vue
+++ versao-inicial/frontend/src/components/template/Header.vue
@@ -4,15 +4,20 @@
             <i class="fa fa-lg" :class="icon"></i>
         </a>
         <h1 class="title">{{title}}</h1>
+        <UserDropdown v-if="!hideUserDropdown" />
     </header>
 </template>
 
 <script>
+import UserDropdown from './UserDropdown'
+
 export default {
     name:'Header',
+    components: { UserDropdown },
     props:{
         title: String,
-        hideToggle: Boolean
+        hideToggle: Boolean,
+        hideuserDropdown: Boolean
     }, 
     computed:{
         icon(){
@@ -59,6 +64,7 @@ export default {
         align-items: center;
     }
     header.header > a.toggle:hover{
+        color: #FFF;
         background-color: rgba(0, 0, 0, 0.2);
     }
 </style>
